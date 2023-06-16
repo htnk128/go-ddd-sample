5b7563ac0811bd09d554e7235e86261c5eab3ace
htnk128 <hiroaki.tanaka128@gmail.com>
2023-06-16 10:02:52 +0900

    Fix

--- pkg/address/adapter/controller/resource/address_response.go
+++ pkg/address/adapter/controller/resource/address_response.go
@@ -5,7 +5,7 @@ type AddressResponse struct {
 	OwnerID       string  `json:"owner_id"`
 	FullName      string  `json:"full_name"`
 	ZipCode       string  `json:"zip_code"`
-	StateOrRegion string  `json:"state_or_Region"`
+	StateOrRegion string  `json:"state_or_region"`
 	Line1         string  `json:"line1"`
 	Line2         *string `json:"line2"`
 	PhoneNumber   string  `json:"phone_number"`
